0b7ce71229483b78f1377409cf10b053dcd73f33
mengda636431 <mengda63641@outlook.com>
2023-03-17 09:03:54 +0800

    添加项目文件。

---
+++ fortran.f90
@@ -0,0 +1,25 @@
+!  Console.f90 
+!
+!  FUNCTIONS:
+!  Console - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: Console
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program Console
+
+    implicit none
+
+    ! Variables
+
+    ! Body of Console
+    print *, 'Hello World'
+
+    end program Console
+
